2d4de1972f355bcfc8be68f12bbfe8daa654ea1b
JulianH99 <juliancorredor99@gmail.com>
2024-07-17 18:53:11 -0500

    some aliases and config for windows

--- dot_config/awesome/rc.lua
+++ dot_config/awesome/rc.lua
@@ -645,19 +645,23 @@ ruled.client.connect_signal("request::rules", function()
 				"Wpa_gui",
 				"veromix",
 				"xtightvncviewer",
+				"pavucontrol",
 			},
 			-- Note that the name property shown in xprop might be set slightly after creation of the client
 			-- and the name shown there might not match defined rules here.
 			name = {
 				"Event Tester", -- xev.
+				"Save As", -- firefox save file dialog
+				"Save Image", -- firefox save image dialog
 			},
 			role = {
 				"AlarmWindow", -- Thunderbird's calendar.
 				"ConfigManager", -- Thunderbird's about:config.
 				"pop-up", -- e.g. Google Chrome's (detached) Developer Tools.
+				"GtkFileChooserDialog", -- general gtk file dialog
 			},
 		},
-		properties = { floating = true },
+		properties = { floating = true, placement = awful.placement.centered },
 	})
 
 	-- Set Firefox to always map on the tag named "2" on screen 1.
